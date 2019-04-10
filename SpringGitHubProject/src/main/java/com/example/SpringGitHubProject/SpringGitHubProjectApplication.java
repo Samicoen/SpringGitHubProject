f215b67361d9adb96e2677233f824ae52b307049
Samicoen <skflay@mum.edu>
2019-04-10 13:51:34 -0500

    First Commit.

---
+++ SpringGitHubProject/src/main/java/com/example/SpringGitHubProject/SpringGitHubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringGitHubProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitHubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitHubProjectApplication.class, args);
+	}
+
+}
